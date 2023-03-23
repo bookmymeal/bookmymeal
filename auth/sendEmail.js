a097bad9f6f049f686cb32dccf602e7983faa69c
DHAVAL <dhaval@DHAVALtekiMacBook-Air.local>
2023-03-23 20:59:22 +0530

    env variable chnaged

--- auth/sendEmail.js
+++ auth/sendEmail.js
@@ -1,11 +1,13 @@
 const nodemailer = require('nodemailer')
 const sendEmailPass = process.env.emailPass
 const email = process.env.email
+const smtpRelayHost = process.env.smtpRelayHost
+const emailSender = process.env.emailSender
 
 async function sendEmail(to, subject, link) {
 
   let transporter = nodemailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
+    host: smtpRelayHost,
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -16,7 +18,7 @@ async function sendEmail(to, subject, link) {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: email, // sender address
+    from: emailSender, // sender address
     to: to, // list of receivers
     subject: subject, // Subject line
     text: link, // plain text body
